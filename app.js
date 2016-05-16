51cea594d4846e5eb77802d3f1a8766a17be99a6
Lucy Follansbee <lfollansbee@gmail.com>
2016-05-16 15:24:22 -0600

    finish jquery get of input text and run search function

--- app.js
+++ app.js
@@ -9,20 +9,30 @@ function conditional(kind, input){
 }
 
 function search(input){
-  for(var i = 0; i<wines.length; i++){
+  for(var i = 0; i < wines.length; i++){
     name = wines[i];
     currentType = wine[name].types;
     cheesePair = wine[name].pairs;
     inSystem = conditional(currentType, input);
     if (inSystem === true){
-      console.log(input);
+      console.log(name);
       return input;
       return name;
     }
   }
 }
-search("champagne");
 
-var inputPairs = wine[name].pairs;
-var inputPerf = wine[name].perf;
-console.log(inputPairs + inputPerf)
+// var inputPairs = wine[name].pairs;
+// var inputPerf = wine[name].perf;
+// console.log(inputPairs + inputPerf)
+
+$(document).ready(function(){
+  $(".form-group").on("submit", function(){
+    event.preventDefault();
+    wineEntered = $("#wineSearch").val().toLowerCase();
+    $("#inputWine").empty();
+    $("#inputWine").append(wineEntered);
+    console.log(wineEntered);
+    search(wineEntered)
+  })
+});
